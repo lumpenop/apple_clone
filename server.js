36787a224ea5971fee31c5cb029cbf393b5d468b
swk <swk@swkui-MacBookPro.local>
2021-06-07 17:56:31 +0900

    join.html2

--- server.js
+++ server.js
@@ -21,14 +21,14 @@ sequelize.sync({force:false,})
 .then(()=>{console.log('접속완료')})
 .catch(()=>{console.log('접속 실패')})
 
-app.use(express.static(__dirname + 'public'));
+
 nunjucks.configure('views', {
     express:app,
 })
 app.set('view engine', 'html');
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.urlencoded({extended:false}));
 app.use(express.static('public'));
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(session({
     secret:'any',
     resave:true,
