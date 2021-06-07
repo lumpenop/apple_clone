0f24ab059e43d88661a1a792aa0a1059747b2316
LS7875 <tmdgml7875@gmail.com>
2021-06-07 10:12:41 +0900

    basic server update

---
+++ server.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const app = express();
+const nunjucks = require('nunjucks');
+const bodyParser = require('body-parser');
+
+nunjucks.configure('views', {
+    express:app,
+})
+app.set('view engine', 'html');
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/', (req,res)=>{
+    res.render('index.html');
+})
+
+app.listen(3000,()=>{
+    console.log('server start port : 3000')
+})
