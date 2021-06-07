4b9f801f946b4d42d4439ca26daa1d88e76b1ee5
LS7875 <tmdgml7875@gmail.com>
2021-06-07 10:42:45 +0900

    update

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
+require('dotenv').config();
 const express=require('express');
 const app = express();
 const nunjucks = require('nunjucks');
 const bodyParser = require('body-parser');
+const port = process.env.SERVER_PORT || 3000; 
+const main= require('./routers/index.js')
 
 nunjucks.configure('views', {
     express:app,
 })
 app.set('view engine', 'html');
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static('public'));
 
-app.get('/', (req,res)=>{
-    res.render('index.html');
-})
 
-app.listen(3000,()=>{
-    console.log('server start port : 3000')
+app.get('/', main)
+
+app.listen(port,()=>{
+    console.log(`server start port : ${port}`)
 })
