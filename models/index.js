aca9618c1bf45e811ed20fbaf444208f27136798
LS7875 <tmdgml7875@gmail.com>
2021-06-07 11:59:47 +0900

    sequelize & tables update

---
+++ models/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+const initModels = require('./init-models');
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+let models = initModels(sequelize);
+console.log(models);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+db.users = models.users;
+db.items = models.items;
+db.buy = models.buy; 
+
+module.exports = db;
---
+++ models/init-models.js
@@ -0,0 +1,26 @@
+var DataTypes = require("sequelize").DataTypes;
+var _buy = require("./buy");
+var _items = require("./items");
+var _users = require("./users");
+
+function initModels(sequelize) {
+  var buy = _buy(sequelize, DataTypes);
+  var items = _items(sequelize, DataTypes);
+  var users = _users(sequelize, DataTypes);
+
+  buy.belongsTo(items, { as: "buying_item_serial_number_item", foreignKey: "buying_item_serial_number"});
+  items.hasMany(buy, { as: "buys", foreignKey: "buying_item_serial_number"});
+  buy.belongsTo(users, { as: "buying_user", foreignKey: "buying_userid"});
+  users.hasMany(buy, { as: "buys", foreignKey: "buying_userid"});
+
+  buy.removeAttribute('id');
+
+  return {
+    buy,
+    items,
+    users,
+  };
+}
+module.exports = initModels;
+module.exports.initModels = initModels;
+module.exports.default = initModels;
