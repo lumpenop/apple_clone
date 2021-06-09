8dc8af645c64dc65b3e71448537d01673a15b3bb
simbian2 <simbianartist@gmail.com>
2021-06-09 18:14:41 +0900

    d

--- routers/search/index.js
+++ routers/search/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const controller = require('./searchController')
 const sequelize = require("sequelize")
 
-router.get('/search_error',controller.search_error);
-router.get('/search_iphone',controller.search_iphone);
-router.get('/search',controller.search_view);
+router.get('/error',controller.search_error);
+router.get('/iphone',controller.search_iphone);
+router.get('/ipad',controller.search_ipad);
+router.post('/search',controller.search_view);
+router.get('/db',controller.db);
 router.post('/search',controller.search);
 
 module.exports = router;
--- routers/search/searchController.js
+++ routers/search/searchController.js
@@ -3,25 +3,39 @@ const sequelize = require("sequelize")
 
 
 let search_error = (req,res)=>{
-    res.render('./search/search_error.html',{
+    res.render('./search/error.html',{
 
     })
 }
 
 let search_iphone = (req,res)=>{
-    res.render('./search/search_iphone.html',{
+    res.render('./search/iphone.html',{
 
     })
 }
 
-let search_view = (req,res)=>{
-    res.render('./search/search.html',{
+let search_ipad = (req,res)=>{
+    res.render('./search/ipad.html',{
+
     })
+}
+let db = async (req,res)=>{
+
+    let result = await items.findAll({})
+    console.log(result)
+    res.render('./search/db.html',{
+        result,
+    })
+}
+let search_view = (req,res)=>{
+    let searchValue = req.body.AppleSearch
+    res.redirect(`/search/${searchValue}`)
 }
 
 let search = (req,res)=>{
     let searchValue = req.body.AppleSearch
-    res.render('./search/search.html',{searchValue:searchValue,})
+
+    res.render(`./search/${searchValue}.html`,{searchValue:searchValue,})
     console.log(searchValue)
 }
 
@@ -29,5 +43,7 @@ module.exports = {
     search_error,
     search,
     search_view,
-    search_iphone
+    search_iphone,
+    search_ipad,
+    db,
 }; 
