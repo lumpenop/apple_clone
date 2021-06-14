a8c073c935acebe315d5a5100396ca95a83a7682
swk <swk@swkui-MacBookPro.local>
2021-06-14 17:54:05 +0900

    pull

---
+++ public/main/section1.js
@@ -0,0 +1,4 @@
+// timer(100);
+// document.querySelector('.section-hero .video-container .startframe').classList.add('hidden');
+// timer(3900);
+// document.querySelector('.section-hero .video-container video').classList.add('hidden');
