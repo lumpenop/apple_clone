86732494e9875dd7d1dfedf16394188866a304a4
swk <swk@swkui-MacBookPro.local>
2021-06-15 20:01:26 +0900

    좌표에 따른 opacity 구해야함

--- public/main/section1.js
+++ public/main/section1.js
@@ -16,15 +16,50 @@ async function imgVideo(){
 
 imgVideo();
 
-
+let beforeWindowHeight = window.innerHeight;
 const changeHandler = () => {
-    console.log(window.innerHeight);
-    document.querySelector(".video-container > .startframe").style.backgroundSize=`${window.innerHeight*1.3}px ${window.innerHeight}px`;
-    document.querySelector(".video-container > video").style.height=String(window.innerHeight)+"px";
-    document.querySelector(".video-container > .endframe").style.backgroundSize=`${window.innerHeight*1.3}px ${window.innerHeight}px`;
-    document.querySelector(".video-container > .endframe").style.height=`${window.innerHeight}px`;
-    document.querySelector(".video-container > .endframe").style.width=`${window.innerHeight * 1.3}px`;
+    let afterWindowHeight = window.innerHeight;
+    
+    plusHeight = (afterWindowHeight - beforeWindowHeight)/afterWindowHeight;
+    console.log(plusHeight)
+
+    const startFrame = document.querySelector(".video-container > .startframe");
+    const video = document.querySelector(".video-container > video");
+    const endFrame = document.querySelector(".video-container > .endframe");
+    const videoContainer = document.querySelector(".video-container");
+    const stickyContent = document.querySelector(".sticky-content");
+
+    startFrame.style.backgroundSize=`${window.innerHeight*1.3}px ${window.innerHeight}px`;
+    video.style.height=String(window.innerHeight)+"px";
+    endFrame.style.backgroundSize=`${window.innerHeight*1.3}px ${window.innerHeight}px`;
+    endFrame.style.height=`${window.innerHeight}px`;
+    endFrame.style.width=`${window.innerHeight * 1.3}px`;
+    videoContainer.style.height=`${window.innerHeight}px`;
+    
+    stickyContent.style.height=String(window.innerHeight)+"px";
 
   };
   
   window.addEventListener("resize", changeHandler);
+
+
+const options = {
+threshold : [0, 0.5, 1]
+}
+const eyebrowText = document.querySelector('.hero-eyebrow-text');
+const eyebrowTop = eyebrowText.getBoundingClientRect().top; 
+
+const io = new IntersectionObserver((entries, observer)=>{
+entries.forEach(entry => {
+    
+    if (entry.intersectionRatio <= 1){
+        eyebrowText.style.opacity = '0';
+    } else if (entry.intersectionRatio > 1){
+        eyebrowText.style.opacity = String(entry.intersectionRatio)*10;
+    } else{
+        eyebrowText.style.opacity = '1';
+    }
+    });},options);
+
+const imgBox = document.querySelector('.endframe');
+io.observe(imgBox);
